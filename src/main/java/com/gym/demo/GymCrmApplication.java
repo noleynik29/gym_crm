f601c1b03945e9f885a484b2f20a16702f1df3c3
Sasha <zagrebalenko@gmail.com>
2024-05-14 18:44:13 +0300

    bug fix

--- src/main/java/com/gym/demo/GymCrmApplication.java
+++ src/main/java/com/gym/demo/GymCrmApplication.java
@@ -1,21 +1,27 @@
 package com.gym.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.EventListener;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class GymCrmApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
-		SpringApplication.run(GymCrmApplication.class, args);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(GymCrmApplication.class);
+		builder.headless(false);
+		ConfigurableApplicationContext context = builder.run(args);
 	}
 
-	@Bean(name="entityManagerFactory")
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-
-		return sessionFactory;
+	@EventListener(ApplicationReadyEvent.class)
+	public void openBrowserAfterStartup() throws IOException, URISyntaxException {
+		Desktop.getDesktop().browse(new URI("http://localhost:8080"));
 	}
 }
